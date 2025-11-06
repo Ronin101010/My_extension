e5862b528d8b88a6c80546f562db97b25be0441b
Oleg <onruchkin@gmail.com>
2025-11-06 16:57:21 +0300

    comment line remover

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import { addAbortListener } from 'events';
+import { toNamespacedPath } from 'path';
 import * as vscode from 'vscode';
 
 // This method is called when your extension is activated
@@ -9,14 +11,30 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "helloextension" is now active!');
-
+	
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('helloextension.helloWorld', () => {
+	const disposable = vscode.commands.registerCommand('helloextension.removecommentlines', () => {
 		// The code you place here will be executed every time your command is executed
+		const editor = vscode.window.activeTextEditor;
+		if (editor) {
+			const document = editor.document;
+			const selection = editor.selection;
+
+			// Get the word within the selection
+			const word = document.getText(selection);
+			// remove not first comment lines
+			const prefixed = word.replace((RegExp)("((\n)( *)(	*)(//.+))+\n", "g"), "\n");
+			// remove first comment lines
+			const fixed = prefixed.replace((RegExp)("((^)( *)(	*)(//.+))+\n", "g"), "");
+			//return fixed text
+			editor.edit(editBuilder => {
+				editBuilder.replace(selection, fixed);
+			});
+		}
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from HelloExtension!');
+		vscode.window.showInformationMessage('Comment lines removed!');
 	});
 
 	context.subscriptions.push(disposable);
